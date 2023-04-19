90af541fd1ee96b8b916904845e7928fb573d987
Scott Feister <sfeister@gmail.com>
2023-04-19 02:25:02 +0000

    Testing out Teensy Rosie and Teensy Pulse generator, working well!

--- streamDevice/teensyrosie.cmd
+++ streamDevice/teensyrosie.cmd
@@ -4,20 +4,20 @@ streamApp_registerRecordDeviceDriver
 
 epicsEnvSet "STREAM_PROTOCOL_PATH","."
 
-drvAsynSerialPortConfigure("teensypulse_ino","/dev/ttyUSB-teensy1.1")
-asynSetOption("teensypulse_ino",0,"baud","115200")
-asynSetOption("teensypulse_ino",0,"bits","8")
-asynSetOption("teensypulse_ino",0,"parity","none")
-asynSetOption("teensypulse_ino",0,"stop","1")
-asynSetOption("teensypulse_ino",0,"clocal","Y")
-asynSetOption("teensypulse_ino",0,"crtscts","N")
+drvAsynSerialPortConfigure("teensyrosie_ino","/dev/ttyUSB-teensy1.2")
+asynSetOption("teensyrosie_ino",0,"baud","115200")
+asynSetOption("teensyrosie_ino",0,"bits","8")
+asynSetOption("teensyrosie_ino",0,"parity","none")
+asynSetOption("teensyrosie_ino",0,"stop","1")
+asynSetOption("teensyrosie_ino",0,"clocal","Y")
+asynSetOption("teensyrosie_ino",0,"crtscts","N")
 
 #drvAsynIPPortConfigure "LO","localhost:40000"
 #vxi11Configure "LO","192.168.1.236",0,0.0,"gpin0"
 
 epicsThreadSleep(5) # empirically necessary to pause here, to avoid communication timeout on first SCPI commands to Arduinos. -SKF April 5 2022
 
-dbLoadRecords "teensypulse.db"
+dbLoadRecords "teensyrosie.db"
 
 #log debug output to file
 #streamSetLogfile StreamDebug.log
