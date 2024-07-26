4867059d396d69a9646e1363807ee31873798009
Scott Feister <sfeister@gmail.com>
2024-07-25 23:52:17 -0700

    Adding a script to start the Teensy USB data server

---
+++ Python/st-diode-teensy-serial-pva-server.cmd
@@ -0,0 +1,13 @@
+#!/bin/bash
+# DATA ACQUISITION PVA SERVER
+
+DOLPHINDAQ_ROOT=/home/debian/dolphindaq
+
+# Use the "daq" virtual environment for Python already configured in debian home directory
+export WORKON_HOME=/home/debian/.virtualenvs
+source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+workon daq
+
+# Run the python script within the dolhindaq folder so it can find its dependencies
+cd ${DOLPHINDAQ_ROOT}/Python
+python diode-teensy-serial-pva-server.py --prefix "TEENSYDIODE-DAQ" --tty "/dev/ttyACM2"
